bdd9df2e493efc5adfdcd19791a2f2a4d7759e3f
Sayan Dey <sayantadeytraining@gmail.com>
2019-09-10 07:53:20 +0530

    Own package installed and executed

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var _ = require('underscore');  //Calling the installed package
+var addM = require('lion-lib-sayan');
 
 var result = _.contains([1,2,3], 1);
 console.log(result);
+
+var newRes = addM.add(2, 3);
+console.log(newRes);
